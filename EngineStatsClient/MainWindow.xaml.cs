6d972e784f47470e036ad60678c1c34575e4b1bc
Alejandro Montaner Roig <alroigcode@gmail.com>
2025-03-11 17:28:45 +0100

    Add project files.

---
+++ EngineStatsClient/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EngineStatsClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int GWL_EXSTYLE = -20;
+        private const int WS_EX_TRANSPARENT = 0x00000020;
+		private const int WS_EX_LAYERED = 0x00080000;
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hwnd, int nIndex, int dwNewLong);
+		[DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hwnd, int nIndex);
+
+		public MainWindow()
+        {
+            InitializeComponent();
+            this.Left = 0;
+            this.Top =  0;
+		}
+    }
+}
